8be6d41e81fb178da1904196011ddfa689a6d36f
rajnagani <rajnagani331@gmail.com>
2025-11-29 00:19:43 +0530

    Enhance PDF cropping functionality to support merging and processing multiple PDFs, returning results as a zip file.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 # PDF CROPER API
 
+from operator import le
 import os
 from fastapi import FastAPI , File, UploadFile,Form,Query,HTTPException 
 from fastapi.responses import FileResponse, StreamingResponse
@@ -8,6 +9,9 @@ from typing import Annotated, List, Optional
 from utils import remove_pdf_whitespace, merge_pdfs, get_indian_datetime, print_datetime_on_label,process_pdf
 from io import BytesIO
 import fitz  # PyMuPDF
+import zipfile
+import datetime
+import base64
 
 
 
@@ -27,30 +31,73 @@ async def crop_pdf_editor(
         }
 
         # STEP 1: READ ALL PDFs INTO MEMORY
-        file_docs = []
+        input_pdf = []
         for file in files:
             file_bytes = await file.read()
             doc = fitz.open(stream=file_bytes, filetype="pdf")
-            file_docs.append(doc)
+            input_pdf.append(doc)
 
-        # STEP 2: MERGE IN MEMORY
+        """ If merge is True then merge all PDF and apply process_pdf() on merged PDF and return single PDF""" 
         if merge:
             merged_doc = fitz.open()
-            for d in file_docs:
-                merged_doc.insert_pdf(d)
-        else:
-            merged_doc = file_docs[0]
+            for pdf in input_pdf:
+                merged_doc.insert_pdf(pdf)
+        
 
-        # STEP 3: APPLY process_pdf()
-        merged_bytes = merged_doc.tobytes()
-        final_doc = process_pdf(merged_bytes, settings)
+            merged_bytes = BytesIO(merged_doc.tobytes())
+            final_doc = process_pdf(merged_bytes, settings)
 
-        # STEP 4: RETURN PDF
+            return StreamingResponse(
+                BytesIO(final_doc.tobytes()),
+                media_type="application/pdf",
+                headers={"Content-Disposition": "attachment; filename=processed.pdf"}
+            )
+        
+        """ If merge is False & and User pass one PDF then apply process_pdf() on each PDF and return zip of all processed PDFs"""
+        if len(input_pdf) == 1:
+            single_bytes = BytesIO(input_pdf[0].tobytes())
+            final_doc = process_pdf(single_bytes, settings)
+
+            return StreamingResponse(
+                BytesIO(final_doc.tobytes()),
+                media_type="application/pdf",
+                headers={"Content-Disposition": "attachment; filename=processed.pdf"}
+            )
+
+
+        """ If merge is False & and User pass multiple PDFs then apply process_pdf() on each PDF and return zip of all processed PDFs"""
+        zip_buffer = BytesIO()
+        with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+
+            for idx, pdf in enumerate(input_pdf):
+                pdf_bytes = BytesIO(pdf.tobytes())
+                processed_doc = process_pdf(pdf_bytes, settings)
+                processed_bytes = processed_doc.tobytes()
+                zip_file.writestr(f"processed_{idx + 1}.pdf", processed_bytes)
+
+        zip_buffer.seek(0)
+        print(zip_buffer)
         return StreamingResponse(
-            BytesIO(final_doc.tobytes()),
-            media_type="application/pdf",
-            headers={"Content-Disposition": "attachment; filename=processed.pdf"}
+            zip_buffer,
+            media_type="application/zip",
+            headers={"Content-Disposition": "attachment; filename=processed_pdfs.zip"}
         )
 
+        # files_data = []
+
+        # for idx, pdf_bytes in enumerate(input_pdf, start=1):
+
+        #     processed_doc = process_pdf(pdf_bytes, settings)
+
+        #     pdf_raw_bytes = processed_doc.tobytes()
+        #     encoded = base64.b64encode(pdf_raw_bytes).decode("utf-8")
+
+        #     # FINAL: Add to JSON
+        #     files_data.append({
+        #         "name": f"processed_{idx}.pdf",
+        #         "data": encoded
+        #     })
+
+        # return {"files": files_data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
